330687f2f0b0028e8da8764e5eeba7454b751129
Evgeniy M <evg.milushkin@gmail.com>
2024-04-18 23:08:53 +0300

    update NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,13 @@ export default function NavBar({ logout, currentUser }) {
 	return (
 		<div className={styles['navbar']}>
 			<a href='' className={styles['active']}>Поиск фильмов</a>
-			{currentUser && <a href=''>Мои фильмы <Badge type='badge-primary' count="2" /></a>}
-			{currentUser && <a href=''>{currentUser.username} <Icon srcImage="/user.svg" /></a>}
-			{currentUser && <a href='' onClick={logout}>Выйти</a>}
+			{currentUser &&
+				<>
+					<a href=''>Мои фильмы <Badge type='badge-primary' count="2" /></a>
+					<a href=''>{currentUser.username} <Icon srcImage="/user.svg" /></a>
+					<a href='' onClick={logout}>Выйти</a>
+				</>
+			}
 			{!currentUser && <a href=''>Войти <Icon srcImage="/login.svg" /></a>}
 		</div >
 	);
